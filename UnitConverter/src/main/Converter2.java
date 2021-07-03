ae10671dffaa72ab30c47424f945da5ec30fa702
johnm0724 <36236210+johnm0724@users.noreply.github.com>
2021-07-02 21:19:40 -0400

    Add files via upload

--- UnitConverter/src/main/Converter2.java
+++ UnitConverter/src/main/Converter2.java
@@ -2,13 +2,17 @@ package main;
 
 import java.util.Scanner;
 
+
+
+
 public class Converter2 {
 
 	
-	
+	VolCon vc = new VolCon();
+	DisCon dc = new DisCon();
 	
 public static void main(String[] args){
-		
+		Converter2 con = new Converter2();
 		Scanner scan = new Scanner(System.in);
 		
 		
@@ -17,50 +21,41 @@ public static void main(String[] args){
 		System.out.println("2. Distance Converter");
 		String menuSelection1 = scan.nextLine();
 		
-	while(menuSelection1 != "4") {
+	while(menuSelection1 != "3") {
 			
-		  if( "1".equals(menuSelection1)) {
-				
-			//some code to call a method to work with vol converter
-			  
-			//String menuSelection1 = scan.nextLine(); user input for method
-			  
-			    if ("1".equals (menuSelection1)){
-			    
+		  if( "1".equals(menuSelection1)) {  
 			    System.out.println("Volume Converter menu:");
-			    System.out.println("1 Cups to Teaspoons:");
-			    System.out.println("2 US gallons to Imperial gallons:");
+			    System.out.println("1. Cups to Teaspoons:");
+			    System.out.println("2. US gallons to Imperial gallons:");
+			    String line = scan.nextLine();
 			    
-			    
-			    
-			         if("2".equals(menuSelection1)){
-			        System.out.println("You have picked Cups to Teaspoons:");
-			         System.out.println("Enter number of cups to convert:");
-			       //this line will be replaced with a method
-			         String line = scan.nextLine();   
-			         
-			  }
 			   
-			        if(some condition is true ){
-			          System.out.println("You have picked US gallons to Imperial gallons:");
-			         System.out.println("Enter number of gallons to convert:");
-			         String line = scan.nextLine();   this line will be replaced with a method
-			    }
-			   
-			}
-		  else if( "2".equals(menuSelection1)) {
-			//some code to call a method to work with dis converter
-			  //String menuSelection1 = scan.nextLine(); user input for method
+			         if("1".equals(line)){
+			            System.out.println("You have picked Cups to Teaspoons:");
+			            System.out.println("Enter number of cups to convert:");
+			           //this line will be replaced with a method
+			            String line1 = scan.nextLine();
+			            con.vc.cup(line1);
+			           }
+			         else if("2".equals(line) ){
+			            System.out.println("You have picked US gallons to Imperial gallons:");
+			            System.out.println("Enter number of gallons to convert:");
+			            String line1 = scan.nextLine();  
+			            con.vc.gallons(line1);
+			            }
+			    
+		              
 		  }
-		 	 
-		  else {
-			  
-			  System.out.println("you have left the loop!");
-			  menuSelection1 = "5";
+		 else if( "2".equals(menuSelection1)) {
+			   
+    	         System.out.println("Welcome to Distance Converter");
+    	         System.out.println("Enter the number of miles to convert:");
+    	         String line2 = scan.nextLine();
+    	         con.dc.miles(line2);
 		  }
 		  
 		  
 		
 	
-	}}}
+		  }}}
 
--- UnitConverter/src/main/VolCon.java
+++ UnitConverter/src/main/VolCon.java
@@ -1,29 +1,25 @@
 package main;
 
+
+
+
 public class VolCon {
 
 	
-	public VolCon() {
-	//System.out.println("You picked number:" + menuSelection1);
-	//System.out.println("Welcome to the Cups to Teaspoons converter how many cups do you want to convert.");
-	//String line1 = scan.nextLine();
-	//int cupTemp = Integer.parseInt(line1);
-
-	//System.out.println(line1 + " Cups are " +  (cupTemp * 48)  + " Teaspoons." );
-    // System.out.println("Please enter a number 1 to 4");
-     // menuSelection1 = scan.nextLine();
+	public void cup(String input) {
+		
+	  
+     int cupTemp = (Integer.parseInt(input ) * 48);
+	    
+	    System.out.println( input + "cup(s) converts to " + cupTemp + " Teaspoon"  );
 	}
 	
+	public void gallons(String input) {
 	
-	//System.out.println("You picked number:" + menuSelection1);
-	//System.out.println("Welcome to the US Gallons to Imperial Gallons converter how many us gallons do you want to convert.");
-	//String line1 = scan.nextLine();
-	//int gallonTemp = Integer.parseInt(line1);//change calculation 
-
-	//System.out.println(line1 + " Gallons are " +  (gallonTemp * 0.83267384)  + " Imperial Gallons." );
+	  int gallonTemp = (int) (Integer.parseInt(input) * 0.83267384);
+	   System.out.println( input + " gallon(s) converts to " + gallonTemp + "Imperial gallons");
 	
-	// System.out.println("Please enter a number 1 to 4");
-    //  menuSelection1 = scan.nextLine();
+	}
 }
 
 
